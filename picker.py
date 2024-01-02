2965fa98e60751e7db966e294ed672d3a8522b52
Austin Hendricks <hendraustin@gmail.com>
2024-01-02 12:44:37 -0800

    Refactor to utilize Yelp Fusion API

--- picker.py
+++ picker.py
@@ -1,35 +1,67 @@
+import math
 import random
 import argparse
-
-from restaurants import restaurants
-
-
-def get_random_restaurant(args: list) -> str:
-    restaurants_list = []
-
-    for restaurant in restaurants:
-        if args.healthy is False and args.sitdown is False:
-            restaurants_list.append(restaurant["Name"])
-        elif (args.healthy is True and args.healthy == restaurant["Healthy"]) and (
-            args.sitdown is True and args.sitdown == restaurant["Sit down"]
-        ):
-            restaurants_list.append(restaurant["Name"])
-        elif (
-            args.healthy is True
-            and args.healthy == restaurant["Healthy"]
-            and args.sitdown is False
-        ):
-            restaurants_list.append(restaurant["Name"])
-        elif (
-            args.sitdown is True
-            and args.sitdown == restaurant["Sit down"]
-            and args.healthy is False
-        ):
-            restaurants_list.append(restaurant["Name"])
-
-    return random.choice(restaurants_list)
+import requests
 
 
+def get_lat_lon() -> int:
+    city = input("Please input your city: ")
+    state = input("Please input your state: ")
+
+    locations = requests.get(f"https://geocode.maps.co/search?q={city}+{state}")
+
+    # Geocode passes multiple potential locations, but they place their best 'guesstimate' in the 0th index
+    lat = locations.json()[0]["lat"]
+    lon = locations.json()[0]["lon"]
+    return lat, lon
+
+
+# Yelp Fusion API has a max of 50 businesses per page that are always sorted in the same order
+# So, we're choosing a random initial starting point (offset) to select a restaurant from
+def get_random_offset_page(lat: int, lon: int, total: int) -> list:
+    max_limit = 50
+    restaurants = []
+
+    random_offset = random.randrange(0, math.floor(total / max_limit) * max_limit)
+    url = f"https://api.yelp.com/v3/businesses/search?term=restaurant&latitude={lat}&longitude={lon}&offset={random_offset}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer ",
+    }
+
+    req = requests.get(url=url, headers=headers)
+
+    businesses = req.json()["businesses"]
+
+    for restaurant in businesses:
+        restaurants.append(restaurant["name"])
+
+    return restaurants
+
+
+def get_list_of_restaurants(lat: int, lon: int) -> int:
+    url = f"https://api.yelp.com/v3/businesses/search?term=restaurant&latitude={lat}&longitude={lon}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer ",
+    }
+    r = requests.get(url=url, headers=headers)
+    total = r.json()["total"]
+
+    restaurants = get_random_offset_page(lat, lon, total)
+
+    return restaurants
+
+
+def get_random_restaurant(restaurants: list) -> str:
+    lat, lon = get_lat_lon()
+
+    restaurants = get_list_of_restaurants(lat, lon)
+
+    return random.choice(restaurants)
+
+
+# WIP: Update to utilize kwargs(?)
 def main():
     parser = argparse.ArgumentParser(description="Select a random restaurant")
     parser.add_argument(
