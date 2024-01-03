89af6822892f631ae036f03aca638913ce8def97
Austin Hendricks <hendraustin@gmail.com>
2024-01-03 13:31:49 -0800

    Remove geocode API request for Yelp location

--- picker.py
+++ picker.py
@@ -1,32 +1,32 @@
+import sys
 import math
 import random
 import argparse
 import requests
 
 
-def get_lat_lon() -> int:
+def get_location() -> str:
     city = input("Please input your city: ")
     state = input("Please input your state: ")
-
-    locations = requests.get(f"https://geocode.maps.co/search?q={city}+{state}")
-
-    # Geocode passes multiple potential locations, but they place their best 'guesstimate' in the 0th index
-    lat = locations.json()[0]["lat"]
-    lon = locations.json()[0]["lon"]
-    return lat, lon
+    return (city + " " + state).replace(" ", "%")
 
 
 # Yelp Fusion API has a max of 50 businesses per page that are always sorted in the same order
 # So, we're choosing a random initial starting point (offset) to select a restaurant from
-def get_random_offset_page(lat: int, lon: int, total: int) -> list:
+def get_random_offset_page(location: str, total: int) -> list:
     max_limit = 50
+    max_businesses_per_request = 1000
     restaurants = []
 
-    random_offset = random.randrange(0, math.floor(total / max_limit) * max_limit)
-    url = f"https://api.yelp.com/v3/businesses/search?term=restaurant&latitude={lat}&longitude={lon}&offset={random_offset}"
+    if total < max_businesses_per_request:
+        random_offset = random.randrange(0, math.floor(total / max_limit) * max_limit)
+    else:
+        random_offset = random.randrange(0, max_businesses_per_request - 1)
+
+    url = f"https://api.yelp.com/v3/businesses/search?term=restaurant&location={location}&offset={random_offset}"
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer ",
+        "Authorization": "Bearer ",  # API key goes here
     }
 
     req = requests.get(url=url, headers=headers)
@@ -39,24 +39,32 @@ def get_random_offset_page(lat: int, lon: int, total: int) -> list:
     return restaurants
 
 
-def get_list_of_restaurants(lat: int, lon: int) -> int:
-    url = f"https://api.yelp.com/v3/businesses/search?term=restaurant&latitude={lat}&longitude={lon}"
+def get_list_of_restaurants(location: str) -> int:
+    url = (
+        f"https://api.yelp.com/v3/businesses/search?term=restaurant&location={location}"
+    )
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer ",
+        "Authorization": "Bearer ",  # API key goes here
     }
     r = requests.get(url=url, headers=headers)
-    total = r.json()["total"]
 
-    restaurants = get_random_offset_page(lat, lon, total)
+    try:
+        total = r.json()["total"]
+    except KeyError:
+        sys.exit(
+            "No restaurants found, check the city and state are correct for the ORP to facilitate your culinary venture."
+        )
+
+    restaurants = get_random_offset_page(location, total)
 
     return restaurants
 
 
-def get_random_restaurant(restaurants: list) -> str:
-    lat, lon = get_lat_lon()
+def get_random_restaurant() -> str:
+    location = get_location()
 
-    restaurants = get_list_of_restaurants(lat, lon)
+    restaurants = get_list_of_restaurants(location)
 
     return random.choice(restaurants)
 
@@ -78,7 +86,7 @@ def main():
     )
     args = parser.parse_args()
 
-    print(f"The ORP has chosen: {get_random_restaurant(args)}!")
+    print(f"The ORP has chosen: {get_random_restaurant()}!")
 
 
 if __name__ == "__main__":
